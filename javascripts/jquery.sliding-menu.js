9bc4a2cee8d1fc117371f04473f60032cd8832ad
devadatta <devadatta@localhost.localdomain>
2010-02-28 12:19:10 +0530

    Added some more options

--- javascripts/jquery.sliding-menu.js
+++ javascripts/jquery.sliding-menu.js
@@ -1,12 +1,26 @@
 (function(jQuery){
   jQuery.fn.slidingMenu = function(options) {
     options = jQuery.extend({
-      backgroundClass: "Background", initialOpacity: 1, slideSpeed: 1, easing: 'linear', callback: function(){}, backgroundContent: '<div></div>'
+      backgroundClass: "Background", initialOpacity: 1, slideSpeed: 1, easing: 'linear', callback: function(){}, backgroundContent: '<div></div>', activeClass: "ActiveLink", highlightOnClick: false
     }, options);
-    var slideTime = 200 * (1/options.slideSpeed);
-    var initial_height = jQuery(this).find("li:eq(0)").find("a:eq(0)").outerHeight();
-    var initial_width = jQuery(this).find("li:eq(0)").find("a:eq(0)").outerWidth();
-    jQuery(this).prepend('<li class="'+options.backgroundClass+'" style="top:0px; left:0px; position: absolute; z-index: 0; display: none; width:'+initial_width+'px; height:'+initial_height+'px; ">'+options.backgroundContent+'</li>');
+    options.slideTime = 200 * (1/options.slideSpeed);
+    options.initial_height = jQuery(this).find("li:eq(0)").find("a:eq(0)").outerHeight();
+    options.initial_width = jQuery(this).find("li:eq(0)").find("a:eq(0)").outerWidth();
+    jQuery(this).css({
+      'position': 'relative',
+    });
+    jQuery(this).find("li a").css({
+      'display': 'inline-block',
+      'position': 'relative'
+    });
+    jQuery(this).prepend('<li class="'+options.backgroundClass+'" style="top:0px; left:0px; position: absolute; z-index: 0; display: none; width:'+options.initial_width+'px; height:'+options.initial_height+'px; ">'+options.backgroundContent+'</li>');
+    options.initial_top = jQuery(this).find("."+options.backgroundClass).eq(0).position().top;
+    options.initial_left = jQuery(this).find("."+options.backgroundClass).eq(0).position().left;
+    if(options.highlightOnClick) {
+      jQuery(this).find("."+options.backgroundClass).eq(0).css({
+        'display': 'block'
+      });
+    }
     jQuery(this).find("li a").hover(function(){
       var t = jQuery(this).position().top;
       var l = jQuery(this).position().left;
@@ -14,11 +28,19 @@
       var h = jQuery(this).outerHeight();
       jQuery(this).parents("ul:first").find("."+options.backgroundClass).stop().animate({
         opacity: options.initialOpacity, top: t, left: l, width: w, height: h
-      }, slideTime, options.easing, options.callback );
+      }, options.slideTime, options.easing, options.callback );
     }, function(){
-      jQuery(this).parents("ul:first").find("."+options.backgroundClass).stop().animate({
-        opacity: "0"
-      }, 500);
+      if(options.highlightOnClick){
+        jQuery(this).parents("ul:first").find("."+options.backgroundClass).stop().animate({
+          opacity: "1",
+          top: options.initial_top,
+          left: options.initial_left
+        }, 500);
+      }else {
+        jQuery(this).parents("ul:first").find("."+options.backgroundClass).stop().animate({
+          opacity: "0"
+        }, 500);
+      }
     });
     jQuery(this).find("li a").click(function(){
       jQuery(this).parents("ul:first").find("."+options.backgroundClass).stop().animate({
@@ -26,6 +48,12 @@
       }, 100).animate({
         opacity: options.initialOpacity
       }, 300);
+      jQuery(this).parents("ul:first").find("li a").removeClass(options.activeClass);
+      jQuery(this).addClass(options.activeClass);
+      if(options.highlightOnClick) {
+        options.initial_top = jQuery(this).position().top;
+        options.initial_left = jQuery(this).position().left;
+      }
     });
   }
 })(jQuery);
